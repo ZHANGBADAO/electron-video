e81d8f7b1a6b92f1b762612f7bc9362f6b3edbec
Zhang XiaoTong <1039146267@qq.com>
2022-04-29 15:07:07 +0800

    防止多开

--- app/main/index.js
+++ app/main/index.js
@@ -6,18 +6,37 @@ const initMenu = require('./menu')
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
 app.disableHardwareAcceleration()
 
-app.whenReady().then(() => {
-  const win = createWindow()
-  initMenu()
-  // initPrinters(win)
-})
+let mainWindow = null //主窗口
 
+// 判断是否是单个实例 防止多开
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+  // 是第二个实例，则退出
+  app.quit()
+} else {
+  // 是第一个实例，则初始化
+  // 监听第二个实例的启动
+  app.on('second-instance', () => {
+    // 使第一个实例获得焦点。
+    if (mainWindow.isMinimized()) mainWindow.restore()
+    mainWindow.focus()
+  })
+
+  // 创建 BrowserWindow，加载应用的其它功能，等等...
+  app.whenReady().then(() => {
+    initMenu()
+    createWindow()
+    // initPrinters(mainWindow)
+  })
+}
+
+// 退出应用
 app.on('window-all-closed', function () {
   app.quit()
 })
 
 function createWindow() {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1500,
     height: 900,
     webPreferences: {
@@ -28,15 +47,13 @@ function createWindow() {
 
   if (app.isPackaged) {
     // 正式环境
-    win.loadFile('dist/index.html')
+    mainWindow.loadFile('dist/index.html')
   } else {
     // 开发环境
-    win.maximize()
-    win.webContents.openDevTools({mode: 'bottom'})
-    win.loadURL('http://localhost:8080')
+    mainWindow.maximize()
+    mainWindow.webContents.openDevTools({mode: 'bottom'})
+    mainWindow.loadURL('http://localhost:8080')
   }
-
-  return win
 }
 
 
